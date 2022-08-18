3fc72f3668452999f3916cfdc3aa866347700a33
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-17 22:54:39 -0300

    finalização

--- main.js
+++ main.js
@@ -7,32 +7,47 @@ const year = document.querySelector('.year');
 const runTime = document.querySelector('.time');
 const plot = document.querySelector('#plot');
 const cast = document.querySelector('#cast');
+const genre = document.querySelectorAll('.genre');
 const errorMessage = document.querySelector('.container__search-error');
 
+const movieContainer = document.querySelector('.container__movie');
+const infosContainer = document.querySelector('.container__infos');
+const messageContainer = document.querySelector('.container__messages');
+
+function showLoadingMessage() {
+    movieContainer.classList.add('ocultar');
+    infosContainer.classList.add('ocultar');
+    messageContainer.classList.remove('ocultar');
+}
+
+function showContainers() {
+    movieContainer.classList.remove('ocultar');
+    infosContainer.classList.remove('ocultar');
+    messageContainer.classList.add('ocultar');
+}
+
 async function movieSearch() {
-    try {
-        let searchMovie = await fetch(`http://www.omdbapi.com/?t=${movie.value.replace(/ /g, '+')}&apikey=97213fd1`);
-        let searchMovieConverted = await searchMovie.json();
+    showContainers();
 
-        img.setAttribute('src', searchMovieConverted.Poster);
-        title.innerText = searchMovieConverted.Title;
-        stars.innerHTML = `<p class="movie-infos__stars"><i class="fa-solid fa-star"></i> ${searchMovieConverted.imdbRating}</p>`
-        year.innerText = searchMovieConverted.Year;
-        runTime.innerText = searchMovieConverted.Runtime;
-        //falta gênero
-        plot.innerText = searchMovieConverted.Plot;
-        cast.innerText = searchMovieConverted.Actors;
+    let searchMovie = await fetch(`http://www.omdbapi.com/?t=${movie.value.replace(/ /g, '+')}&apikey=97213fd1`);
+    let searchMovieConverted = await searchMovie.json();
 
-        if (searchMovieConverted.erro) {
-            console.log('falso');
-            throw Error('Movie not found');
-            //preencher com filme não encontrado
-        }
-        console.log(searchMovieConverted);
-    } catch (erro) {
-        console.log('falso');
+    img.setAttribute('src', searchMovieConverted.Poster);
+    title.innerText = searchMovieConverted.Title;
+    stars.innerHTML = `<p class="movie-infos__stars"><i class="fa-solid fa-star"></i> ${searchMovieConverted.imdbRating}</p>`
+    year.innerText = searchMovieConverted.Year;
+    runTime.innerText = searchMovieConverted.Runtime;
+    plot.innerText = searchMovieConverted.Plot;
+    cast.innerText = searchMovieConverted.Actors;
+
+    for(let i = 0; i < genre.length; i++) {
+        genre[i].innerText = searchMovieConverted.Genre.split(', ')[i];
     }
 
+    if (searchMovieConverted.Error) {
+        showLoadingMessage();
+        messageContainer.innerText = searchMovieConverted.Error;
+    }
 }
 
 searchBtn.addEventListener('click', () => {
@@ -43,6 +58,8 @@ searchBtn.addEventListener('click', () => {
     else {
         errorMessage.style.display = 'none';
         movie.style.borderColor = 'var(--text-title)';
+
+        showLoadingMessage();
         movieSearch();
     }
 
